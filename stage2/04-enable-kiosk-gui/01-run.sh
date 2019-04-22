498945b4cb9c55aed79b51acaa6e978ef12f36b5
Daniel Strommen <nerdile@outlook.com>
2019-04-22 16:44:10 -0700

    Lock kiosk user to startx with no cursor

--- stage2/04-enable-kiosk-gui/01-run.sh
+++ stage2/04-enable-kiosk-gui/01-run.sh
@@ -2,4 +2,9 @@
 
 install -m 755 files/autostart "${ROOTFS_DIR}/etc/xdg/openbox/autostart"
 install -m 755 files/.bash_profile "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bash_profile"
+install -m 755 files/startx_nocursor "${ROOTFS_DIR}/usr/local/bin/startx_nocursor"
+
+on_chroot <<EOF
+	usermod -s /usr/local/bin/startx_nocursor ${FIRST_USER_NAME}
+EOF
 
